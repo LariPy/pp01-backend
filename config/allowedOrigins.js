e6c45a672d35d40f958e2d25ef38b74d60217f57
oma nimi <em.ail@email.com>
2022-09-07 14:23:41 +0300

    comment

---
+++ config/allowedOrigins.js
@@ -0,0 +1,12 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.practiceurl.com',
+    'https://practiceurl.com'
+]
+
+module.exports = allowedOrigins
+
+//practice url
+//it's the "clients" website
+//need to allow both with www and without www
+//hence the two links
